c18754f4304e3a8c925d41af265ba2faac543afa
Alexander Pliskin <alexnetmore@gmail.com>
2019-09-01 19:44:54 +0300

    Corner peak U-Net && predicted coordinates

--- dicty/prepare_data.py
+++ dicty/prepare_data.py
@@ -5,6 +5,7 @@ loops_info = pd.read_excel('dicty_loop_positions_Chr1_Chr5_Chr6.xlsx')
 loops_info['x'] = loops_info['x'].apply(lambda x: int(x - 1))
 loops_info['y'] = loops_info['y'].apply(lambda x: int(x - 1))
 
+
 def substact_are_and_mask_and_save_to_tiff(start, end):
     """
         got start and end position of an image
@@ -43,12 +44,15 @@ def prepare_data(image_size, chromosomes=[1],
 
         # create mask array
         mask = np.zeros_like(arr)
+
         for idx, loop in loops.iterrows():
             if not is_mask_corner_peak:
                 mask[int(loop.x):int(loop.y), int(loop.x):int(loop.y)] = 1
             else:
-                mask[int(loop.x - mask_window):int(loop.x + mask_window), int(loop.y - mask_window):int(loop.y + mask_window)] = 1
-                mask[int(loop.y - mask_window):int(loop.y + mask_window), int(loop.x - mask_window):int(loop.x + mask_window)] = 1
+                mask[int(loop.x - mask_window):int(loop.x + mask_window),
+                int(loop.y - mask_window):int(loop.y + mask_window)] = 1
+                mask[int(loop.y - mask_window):int(loop.y + mask_window),
+                int(loop.x - mask_window):int(loop.x + mask_window)] = 1
 
         i = image_size
         step = image_size
@@ -60,7 +64,7 @@ def prepare_data(image_size, chromosomes=[1],
             area[area == -np.inf] = 0
             area_mask = mask[start:end, start:end]
 
-            i += int(step/2)
+            i += int(step / 2)
             images.append(np.reshape(area, (image_size, image_size, 1)))
             masks.append(np.reshape(area_mask, (image_size, image_size, 1)))
 
--- rao/prepare_data.py
+++ rao/prepare_data.py
@@ -3,28 +3,33 @@ import pandas as pd
 
 resolution = 10 ** 4
 loops_info = pd.read_csv('GSE63525_GM12878_primary+replicate_HiCCUPS_looplist.txt', sep='\t',
-                             dtype = {'x1': np.long, 'x2': np.long, 'y1': np.long, 'y2': np.long})
-loops_info[['x1', 'x2', 'y1', 'y2', 'centroid1', 'centroid2', 'radius']] = loops_info[['x1', 'x2', 'y1', 'y2', 'centroid1', 'centroid2', 'radius']].apply(
-    lambda x : (x / resolution).astype('int64')
+                         dtype={'x1': np.long, 'x2': np.long, 'y1': np.long, 'y2': np.long})
+loops_info[['x1', 'x2', 'y1', 'y2', 'x', 'y', 'radius']] = loops_info[
+    ['x1', 'x2', 'y1', 'y2', 'centroid1', 'centroid2', 'radius']].apply(
+    lambda x: (x / resolution).astype('int64')
 )
 
-def filter_loops(loops, from_percentile = 0, to_percentile = 90):
-	loops_from = np.percentile((loops['y1'] - loops['x1']), from_percentile)
-	loops_to = np.percentile((loops['y1'] - loops['x1']), to_percentile)
-	l = loops[(loops['y1'] - loops['x1']) > loops_from]
-	loops_d = l[(l['y1'] - l['x1']) < loops_to]
 
-	return loops_d
+def filter_loops(loops, from_percentile=0, to_percentile=90):
+    loops_from = np.percentile((loops['y1'] - loops['x1']), from_percentile)
+    loops_to = np.percentile((loops['y1'] - loops['x1']), to_percentile)
+    filtered = loops[(loops['y1'] - loops['x1']) > loops_from]
+    return filtered[(filtered['y1'] - filtered['x1']) < loops_to]
 
-def prepare_data(image_size, chromosomes_idx = [1], is_log = False, is_norm_over_diagonals = True):
+
+def prepare_data(image_size, chromosomes_idx=[1],
+                 is_log=False,
+                 is_norm_over_diagonals=True,
+                 is_mask_corner_peak=False,
+                 mask_window=1):
     images = []
     masks = []
 
-    loops = filter_loops(loops_info)
+    loops_filtered = filter_loops(loops_info)
 
     for chr_num in chromosomes_idx:
         chr = 'chr' + str(chr_num)
-        
+
         path = 'arrs/10kb_' + chr
         if is_norm_over_diagonals:
             path += '_norm'
@@ -33,15 +38,19 @@ def prepare_data(image_size, chromosomes_idx = [1], is_log = False, is_norm_over
         path += '.npy'
 
         arr = np.load(path)
-   
-        loops_positions = loops[loops_info.chr1 == str(chr_num)]
+        loops = loops_filtered[loops_info.chr1 == str(chr_num)]
 
         # create mask array
         mask = np.zeros_like(arr)
-        
-        for index, row in loops_positions.iterrows():
-            x1, x2, y1, y2 = row[['x1', 'x2', 'y1', 'y2']]
-            mask[x1:y1, x2:y2] = 1
+
+        for idx, loop in loops.iterrows():
+            if not is_mask_corner_peak:
+                mask[int(loop.x):int(loop.y), int(loop.x):int(loop.y)] = 1
+            else:
+                mask[int(loop.x - mask_window):int(loop.x + mask_window),
+                int(loop.y - mask_window):int(loop.y + mask_window)] = 1
+                mask[int(loop.y - mask_window):int(loop.y + mask_window),
+                int(loop.x - mask_window):int(loop.x + mask_window)] = 1
 
         i = image_size
         step = image_size
@@ -53,13 +62,13 @@ def prepare_data(image_size, chromosomes_idx = [1], is_log = False, is_norm_over
             area[area == -np.inf] = 0
             area_mask = mask[start:end, start:end]
 
-            i += step
-            images.append(np.reshape(area, (image_size,image_size,1)))
-            masks.append(np.reshape(area_mask, (image_size,image_size,1)))
+            i += int(step / 2)
+            images.append(np.reshape(area, (image_size, image_size, 1)))
+            masks.append(np.reshape(area_mask, (image_size, image_size, 1)))
 
         i -= step
 
-        #get the last part of an array
+        # get the last part of an array
         if i < arr.shape[0]:
             start, end = arr.shape[0] - step, arr.shape[0]
 
@@ -68,10 +77,10 @@ def prepare_data(image_size, chromosomes_idx = [1], is_log = False, is_norm_over
             area[area == -np.inf] = 0
             area_mask = mask[start:end, start:end]
 
-            images.append(np.reshape(area, (image_size,image_size,1)))
-            masks.append(np.reshape(area_mask, (image_size,image_size,1)))
+            images.append(np.reshape(area, (image_size, image_size, 1)))
+            masks.append(np.reshape(area_mask, (image_size, image_size, 1)))
 
-    X = np.array(images)
-    y = np.array(masks)
-        
-    return X,y
+    images = np.array(images)
+    masks = np.array(masks)
+
+    return images, masks
